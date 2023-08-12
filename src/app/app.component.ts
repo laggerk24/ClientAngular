3c6f92bcd6965afdefeb8ede323691a72bb26166
laggerk24 <laggerk24@gmail.com>
2023-08-12 19:36:32 +0530

    API working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Users } from 'src/Models/models';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent  implements OnInit{
   title = 'ClientAngular';
+  constructor(private http:HttpClient){}
+  ngOnInit(): void {
+    this.getUserData();
+  }
+  getUserData(){
+    this.http.get<Users>('https://localhost:7181/api/Users').subscribe((observer)=>{
+      console.log(observer)
+    },
+    (error)=>
+    console.log(error),
+    ()=>
+    console.log('Finished'))
+  }
 }
